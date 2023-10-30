d7208574aa0bf1bf7e82107fc757cfe4b31f0d05
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-10-30 15:01:34 +0900

    ✅sayGoodbyeのみ代用品とする

--- src/04/02/greet3.test.ts
+++ src/04/02/greet3.test.ts
@@ -1,13 +1,14 @@
 import { greet, sayGoodBye } from './greet'
 
-jest.mock('/greet', () => {
-  sayGoodBye: (name: string) => `Good bye, ${name}`
-})
+jest.mock('./greet', () => ({
+  ...jest.requireActual('./greet'),
+  sayGoodBye: (name: string) => `Good bye, ${name}`,
+}))
 
 test('挨拶を返す（本来の実装ではない）', () =>
   expect(greet('taro')).toBe('Hello! taro.'))
 
 test('さよならを返す（本来の実装ではない）', () => {
   const message = `${sayGoodBye('Taro')} See you.`
-  expect(message).toBe('Good bye, Taro. See you')
+  expect(message).toBe('Good bye, Taro See you.')
 })
