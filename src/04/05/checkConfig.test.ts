60d235c77e1762a523dec127b0e19411f3faad6f
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-11 07:50:04 +0900

    一旦コミット

--- src/04/05/checkConfig.test.ts
+++ src/04/05/checkConfig.test.ts
@@ -1,20 +1,24 @@
-import { checkConfig } from "./checkConfig";
+import { checkConfig } from "./checkConfig"
 
-test("モック関数は実行時引数のオブジェクト検証ができる", () => {
-  const mockFn = jest.fn();
-  checkConfig(mockFn);
-  expect(mockFn).toHaveBeenCalledWith({
-    mock: true,
-    feature: { spy: true },
-  });
-});
+describe('checkConfig', () => {
 
-test("expect.objectContaining による部分検証", () => {
-  const mockFn = jest.fn();
-  checkConfig(mockFn);
-  expect(mockFn).toHaveBeenCalledWith(
-    expect.objectContaining({
+  test('モック関数は実行時引数のオブジェクト検証ができる', () => {
+    const mockFn = jest.fn()
+    checkConfig(mockFn)
+    expect(mockFn).toHaveBeenCalledWith({
+      mock: true,
       feature: { spy: true },
     })
-  );
-});
+  })
+
+  test('expect.objectContainingによる部分検証', () => {
+    const mockFn = jest.fn()
+    checkConfig(mockFn)
+    expect(mockFn).toHaveBeenCalledWith(
+      expect.objectContaining({
+        feature: { spy: true }
+      })
+    )
+  })
+})
+
