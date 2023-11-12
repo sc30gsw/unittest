ec6594ee99bae36e8afb1f9902ae91392b646e44
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-13 03:43:32 +0900

    テストの実装

--- src/04/06/index.test.ts
+++ src/04/06/index.test.ts
@@ -1,10 +1,83 @@
 import * as fetchers from '../fetchers'
-import { httpError } from '../fetchers/fixtures'
+import { postMyArticle } from '../fetchers'
+import { httpError, postMyArticleData } from '../fetchers/fixtures'
 import type { ArticleInput } from '../fetchers/type'
+import { checkLength } from '.'
+
+jest.mock('../fetchers')
 
 const mockPostArticle = (input: ArticleInput, status = 200) => {
   if (status > 299)
     return jest
       .spyOn(fetchers, 'postMyArticle')
       .mockRejectedValueOnce(httpError)
+
+  try {
+    checkLength(input.title)
+    checkLength(input.body)
+    return jest
+      .spyOn(fetchers, 'postMyArticle')
+      .mockResolvedValue({ ...postMyArticleData, ...input })
+  } catch (err) {
+    return jest
+      .spyOn(fetchers, 'postMyArticle')
+      .mockRejectedValueOnce(httpError)
+  }
 }
+
+const inputFactory = (input?: ArticleInput) => {
+  return {
+    tags: ['testing'],
+    title: 'TypeScriptを使ったテストの書き方',
+    body: 'テストを書くとき、TypeScriptを使うことで、テストの保守性が向上します。',
+    ...input,
+  }
+}
+
+describe('postMyArticle', () => {
+  test('バリデーションに成功した場合、成功レスポンスが返る', async () => {
+    // バリデーションに通過する入力値を用意
+    const input = inputFactory()
+    // 入力値を含んだ成功レスポンスが返るよう、モックを施す
+    const mock = mockPostArticle(input)
+    // テスト対象の関数に、inputを与えて実行
+    const data = await postMyArticle(input)
+    // 取得したデータに、入力内容が含まれているか検証
+    expect(data).toMatchObject(expect.objectContaining(input))
+    // モック関数が呼び出されたかを検証
+    expect(mock).toBeCalled()
+  })
+
+  test('バリデーションに失敗した場合、rejectされる', async () => {
+    expect.assertions(2)
+    // バリデーションに通過しない入力値を用意
+    const factory = inputFactory()
+    const input = inputFactory({ ...factory, title: '', body: '' })
+    // 入力値を含んだレスポンスが返るよう、モックを施す
+    const mock = mockPostArticle(input)
+    // バリデーションに通過せずrejectされる
+    try {
+      await postMyArticle(input)
+    } catch (err) {
+      // エラーオブジェクトをもってrejectされたことを検証
+      expect(err).toMatchObject({ err: { message: expect.anything() } })
+      // モック関数が呼び出されたことを確認
+      expect(mock).toHaveBeenCalled()
+    }
+  })
+
+  test('データ取得に失敗した場合、rejectsされる', async () => {
+    expect.assertions(2)
+    // バリデーションに通過する入力値を用意
+    const input = inputFactory()
+    // 失敗レスポンスが返るようモックを施す
+    const mock = mockPostArticle(input, 500)
+    // rejectされるか検証
+    try {
+      await postMyArticle(input)
+    } catch (err) {
+      expect(err).toMatchObject(httpError)
+      expect(mock).toBeCalled()
+    }
+  })
+})
--- src/04/07/index.test.ts
+++ src/04/07/index.test.ts
@@ -1,26 +1,21 @@
 import { greetByTime } from '.'
 
-describe('greetByTime(', () => {
-  beforeEach(() => {
-    jest.useFakeTimers()
-  })
+describe('greetByTime', () => {
+  beforeEach(() => jest.useFakeTimers())
+  afterEach(() => jest.useRealTimers())
 
-  afterEach(() => {
-    jest.useRealTimers()
-  })
-
-  test('朝は「おはよう」を返す', () => {
-    jest.setSystemTime(new Date(2023, 4, 23, 8, 0, 0))
+  test('指定時間待つと、経過時間をもってresolveされる', () => {
+    jest.setSystemTime(new Date(2023, 11, 13, 0, 0, 0))
     expect(greetByTime()).toBe('おはよう')
   })
 
-  test('昼は「こんにちは」を返す', () => {
-    jest.setSystemTime(new Date(2023, 4, 23, 14, 0, 0))
+  test('指定時間待つと、経過時間を持ってresolveされる', () => {
+    jest.setSystemTime(new Date(2023, 11, 13, 12, 0, 0))
     expect(greetByTime()).toBe('こんにちは')
   })
 
-  test('夜は「こんばんは」を返す', () => {
-    jest.setSystemTime(new Date(2023, 4, 23, 21, 0, 0))
+  test('指定時間待つと、経過時間をもってresolveされる', () => {
+    jest.setSystemTime(new Date(2023, 11, 13, 18, 0, 0))
     expect(greetByTime()).toBe('こんばんは')
   })
 })
