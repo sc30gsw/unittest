b9f78e5d95d3082db779c691493f3452b874cc3e
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-13 07:21:38 +0900

    一覧表示コンポーネントのテスト

--- src/05/04/ArticleListItem.test.tsx
+++ src/05/04/ArticleListItem.test.tsx
@@ -9,15 +9,10 @@ const item: ItemProps = {
   body: 'テストを書く時、TypeScript を使うことで、テストの保守性が向上します…',
 }
 
-test('ID に紐づいたリンクが表示される', () => {
+test('IDに紐づいたリンクが表示される', () => {
   render(<ArticleListItem {...item} />)
   expect(screen.getByRole('link', { name: 'もっと見る' })).toHaveAttribute(
     'href',
     '/articles/howto-testing-with-typescript',
   )
-})
-
-test('Snapshot: 一覧要素が表示される', () => {
-  const { container } = render(<ArticleListItem {...item} />)
-  expect(container).toMatchSnapshot()
 })
